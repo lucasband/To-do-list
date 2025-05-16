117f2c35dfe16482856d955e02493393e0fea96b
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-05-16 16:47:46 -0300

    create: ajustando scss do material

--- to-do-list-Av15/src/app/Pages/home/home.module.ts
+++ to-do-list-Av15/src/app/Pages/home/home.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { NavbarComponent } from 'src/app/Shared/navbar/navbar.component';
+import { SharedModule } from 'src/app/Shared/shared.module';
+
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    NavbarComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
