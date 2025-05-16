088b47ccc05f8e59ba948b79cbb4d4af482a911e
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-05-16 16:07:48 -0300

    fix: update home

--- to-do-list-Av15/src/app/Pages/home/home.module.ts
+++ to-do-list-Av15/src/app/Pages/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavbarComponent } from 'src/app/Shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
