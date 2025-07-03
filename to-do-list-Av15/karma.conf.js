f810344f9bd5659ead5c2124289afbcc68fd45b8
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-07-03 13:44:31 -0300

    fix: adicionando karma.conf

---
+++ to-do-list-Av15/karma.conf.js
@@ -0,0 +1,39 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        client: {
+            clearContext: false 
+        },
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage'),
+            subdir: '.',
+            reporters: [
+                { type: 'html' },
+                { type: 'text-summary' },
+                { type: 'lcov' }
+            ]
+        },
+        reporters: ['progress', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadless'],
+        customLaunchers: {
+            ChromeHeadless: {
+                base: 'Chrome',
+                flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+            }
+        },
+        singleRun: false,
+        restartOnFileChange: true
+    });
+};
